4fba436e5f4b3cead623abf133d1f6dcfe8d1ed7
Silas Vriend <spvriend@edu.uwaterloo.ca>
2017-04-28 14:34:58 -0400

    Changed function aliases. Added comments to demo.py.
    Added explanatory comments to demo.py.
    Fixed y-axis limits issue with SpecPlot.offset_and_scale().

--- demo.py
+++ demo.py
@@ -3,6 +3,27 @@
 Silas Vriend. Hawthorn Research Group. Winter 2017.
 University of Waterloo. Department of Physics of Astronomy.
 
+This script makes use of the function aliases defined within the SpecPlot class.
+The aliases are as follows:
+
+    plR = plot_scan_range
+    plMT = plot_multiple_traces
+    Lfit = lorentz_fit
+    zero2one = offset_and_scale
+    
+    setTL = set_title
+    setXL = set_xlabel
+    setYL = set_ylabel
+    setCBL = set_clb_label
+    
+    appTL = title_append
+    appXL = xlabel_append
+    appYL = ylabel_append
+
+Note that any changes to the labels must occur after any of the first four
+functions listed above are called. Otherwise, the changes to the labels will
+be undone.
+
 """
 
 import hawpy as hp
@@ -15,94 +36,92 @@ __verbose__ = False
 LNSCO = hp.SpecDataFile('LNSCO')
 YBCO = hp.SpecDataFile('YBCO_XAS')
 
-# Retrieve SpecScan objects by passing SpecDataFile objects the scan number.
+# Create SpecScan objects by passing SpecDataFile objects the scan number.
 SCAN1 = LNSCO[298]
 SCAN2 = YBCO[11]
 SCAN3 = YBCO[9]
 
 
-# STANDARD PLOT TEST.
+# Standard plot. 
+# 
+# The user must provide the y-data label using the exact characters from 
+#   the original spec data file.
 SCAN1.pl(ycol='ChT_REIXS')
 
 
-# STANDARD PLOT TEST without normalization.
+# Standard plot without normalization.
+#
+# To suppress data normalization, the keyword argument mcol must be set equal
+#   to None. The default value of mcol is 'I0_BD3'.
 SCAN1.pl(ycol='ChT_REIXS', mcol=None)
 
 
-# STANDARD PLOT TEST using object oriented methods.
+# Standard plot using object-oriented methods.
 #
-# This allows for editing of the plot title and the axis labels.
-#
-# The SpecPlot class sets the title and x- and y-axis labels automatically,
-#   but they can be modified with the getter and setter methods shown below.
-#
-# Function aliasing could be used to make the commands shorter.
-#   For example, one could add the following lines of code to the body of
-#   the SpecPlot class:
-#
-#       setXL = set_xlabel
-#       setYL = set_ylabel
-#       appXL = xlabel_append
-#       appYL = ylabel_append
-#
+# Using the object-oriented methods demonstrated here allows for the editing of
+#   plot labels. The labels are generated automatically otherwise.
 PLOT3 = SCAN1.pl(ycol='ChT_REIXS')
-PLOT3.set_title('This is a standard plot with a custom title.')
-PLOT3.title_append(' Neat!')
-PLOT3.set_xlabel('Two Theta (degrees)')
-PLOT3.ylabel_append(' (arb.units)')
+PLOT3.setTL('This is a standard plot with a custom title.')
+PLOT3.appTL(' Neat!')
+PLOT3.setXL('Two Theta (degrees)')
+PLOT3.appYL(' (arb.units)')
 
 
-#STANDARD PLOT TEST with Lorentzian fit.
+# Standard plot with Lorentzian fit.
+#
+# The lorentz_fit() method loops through each trace on the plot and tries to
+#   fit the curve to the Lorentz distribution. The fit parameters are displayed
+#   in the legend.
+#
 PLOT4 = SCAN1.pl(ycol='ChT_REIXS')
-PLOT4.lorentz_fit()
+PLOT4.Lfit()
 
 
-# STANDARD PLOT TEST with Lorentzian fit and zero-to-one offset/scale.
-PLOT5 = SCAN1.pl(ycol='ChT_REIXS')
-PLOT5.lorentz_fit()
-PLOT5.scale_offset(124, 125.5)
-
-
-# STANDARD PLOT TEST with multiple scans.
+# Standard plot with multiple scans.
+#
+# Ordering the scans by temperature is up to the user. The colour gradient
+#   will flow from purple at the beginning of the scan list to red at the end
+#   of the scan list.
+#
 SCANLIST = [173, 169, 165, 161, 155, 151, 147, 143, 139,
             135, 103, 131, 127, 125, 121, 117, 109, 113]
 
 PLOT6 = hp.SpecPlot()
-PLOT6.plotMT(LNSCO, SCANLIST)
-PLOT6.set_title('This is multiple scans, coloured in order.')
+PLOT6.plMT(LNSCO, SCANLIST)
+PLOT6.setTL('Multiple scans, coloured in order.')
 
 
-# STANDARD PLOT TEST with a range of scans.
+# Standard plot with multiple scans, then zero-to-one offset-and-scale.
+SCANLIST = [173, 169, 165, 161, 155, 151, 147, 143, 139,
+            135, 103, 131, 127, 125, 121, 117, 109, 113]
+PLOT5 = hp.SpecPlot()
+PLOT5.plMT(LNSCO, SCANLIST)
+PLOT5.zero2one(58, 60.5)
+PLOT5.setTL('This is a zero-to-one offset-and-scale test.')
+
+
+# Standard plot with a range of scans.
 PLOT7 = hp.SpecPlot()
-PLOT7.plotR(LNSCO, 6, 9, ycol='MCP_REIXS', mcol=None)
-PLOT7.set_title('This is a range of scans.')
+PLOT7.plR(LNSCO, 6, 9, ycol='MCP_REIXS', mcol=None)
+PLOT7.setTL('This is a range of scans.')
 
 
-# MESH PLOT TEST.
+# Mesh plot.
 SCAN2.pl(ycol='TEY_REIXS')
 
 
-# MESH PLOT TEST without normalization.
+# Mesh plot without normalization.
 SCAN2.pl(ycol='TEY_REIXS', mcol=None)
 
 
-# MESH PLOT TEST using object oriented methods.
-#
-# This allows for editing of the title, colorbar label and axis labels.
-#
-# Note that there is not a get_clb_label or clb_label_append method for the
-#   SpecPlot class. This is because the current implementation of the
-#   matplotlib.colorbar.Colorbar class does not have a get_label() method.
-#
+# Mesh plot using object oriented methods.
 PLOT10 = SCAN3.pl(ycol='TEY_REIXS')
-PLOT10.set_title('This is a mesh plot with a custom title.')
-PLOT10.title_append(' Wow!')
-PLOT10.set_clb_label('TEY_REIXS / I0_BD3 (arb. units)')
-PLOT10.xlabel_append(' (mm)')
-PLOT10.ylabel_append(' (mm)')
+PLOT10.setTL('This is a mesh plot with a custom title.')
+PLOT10.appTL(' Wow!')
+PLOT10.setCBL('TEY_REIXS / I0_BD3 (arb. units)')
+PLOT10.appXL(' (mm)')
+PLOT10.appXL(' (mm)')
 
 
 # Calling plt.show() at the very end shows all of the plots at once.
-#   Calling it earlier would mean that the plots created after the call to
-#   plt.show() would not be displayed.
 plt.show()
